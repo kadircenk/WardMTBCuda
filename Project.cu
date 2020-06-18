2cc9fbea16dc9747d8e43acb58c77e4782b6a71b
Kadir Cenk Alpay <kadircenkalpay@gmail.com>
2020-06-18 17:10:14 +0300

    [FINAL] for paper

---
+++ Project.cu
@@ -0,0 +1,573 @@
+//TODO dynamic parallelism kullanamadik kernel icinde kernel. cok kasti. cudaMalloc vs dinamik size'da yapamadik kernel icinde.
+//TODO texture cok vaktimizi aldi. (2 gün totalde) globalden texRef alan yontem imaj piramidinin bazi levellarinda calismadi,
+//TODO benchmark using 2 kernels of histogram finding, and 1 merged kernel.
+//TODO findMedian kernel'ini de gom ustteki merge haline.
+//TODO write'ler olmadan, readler dahil 1080 -> 593.515ms. gtx850m -> 1638.57 ms.
+//TODO preprocessing kisminda img_count tane stream actik, her mtb ebm find kernel'i kendi streaminde. memcpyAsync. gtx850m->1495.27 ms oldu.
+//TODO asil algoritmanin kostugu kismi da her imajin kendi stream'ine koyduk. ama calculateOffsetError if checki sequential hala. simdi multithreaded ekleyecegiz.
+//TODO asil algoritmaya multithread ekledik (image_count tane thread her foto 1 cekirdekte isleniyor) ve -O3 optimizasyon actik (compiler flag), gtx850m -> 1079.43 ms
+//TODO Release modda cesitli compiler optimizasyonlari eklendi:
+//TODO 4k -> (4032x2268) ve 2k -> 1536x2048 dikey.
+//TODO ustteki satir icin: gtx850m -> 343.15 ms (2k fotolar 5 tane), 933.248 ms (4k fotolar, 5 tane).
+//TODO aynisini CPU (fully optimized -O3 var, ama single threaded)-> 1237.07ms (2k fotolar 5 tane) 3885.8ms (4k fotolar 5 tane)
+//TODO CPU multithread yazdik alignment algo. kismina, aksine yavaslatti 4011.04 ms oldu 4k 5 foto. 1401.87 ms 2k 5 foto. (gtx850m)
+
+#include <iostream>
+#include <vector>
+#include <pthread.h>
+#include "cuda_runtime.h"
+#include "kernels.h"
+
+#define THREAD_COUNT 16
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "./stb-master/stb_image_write.h"
+
+using namespace std;
+
+void *calculateOffset(void *args)
+{
+	arguments *_arg;
+	_arg = (arguments *)args;
+
+	int width = _arg->width;
+	int height = _arg->height;
+	int first_index = _arg->first_ind;
+	int second_index = _arg->second_ind;
+	dim3 dimGrid, dimBlock;
+	int tmpWidth = width / (pow(2, PYRAMID_LEVEL - 1));
+	int tmpHeight = height / (pow(2, PYRAMID_LEVEL - 1));
+	int tmpNImageSize = tmpWidth * tmpHeight;
+
+	uint8_t **mtb = _arg->mtb;
+	uint8_t **ebm = _arg->ebm;
+	uint8_t **shifted_mtb = _arg->shifted_mtb;
+	uint8_t **shifted_ebm = _arg->shifted_ebm;
+	cudaStream_t stream = _arg->stream;
+
+	int curr_level = PYRAMID_LEVEL - 1;
+	int curr_offset_x = 0;
+	int curr_offset_y = 0;
+	int offset_return_x = 0;
+	int offset_return_y = 0;
+
+	for (int k = curr_level; k >= 0;
+		 --k, tmpWidth *= 2, tmpHeight *= 2, tmpNImageSize *= 4)
+	{
+		curr_offset_x = 2 * offset_return_x;
+		curr_offset_y = 2 * offset_return_y;
+
+		int min_error = 255 * height * width;
+
+		for (int i = -1; i <= 1; ++i)
+		{
+			for (int j = -1; j <= 1; ++j)
+			{
+				int xs = curr_offset_x + i;
+				int ys = curr_offset_y + j;
+
+				int x_shift = xs, y_shift = ys; //TODO check those
+
+				int j_x, i_y, j_width, i_height;
+
+				if (y_shift < 0)
+				{ //height i
+					i_y = -y_shift;
+					i_height = tmpHeight;
+				}
+				else
+				{
+					i_y = 0;
+					i_height = tmpHeight - y_shift;
+				}
+
+				if (x_shift < 0)
+				{ //width j
+					j_x = -x_shift;
+					j_width = tmpWidth;
+				}
+				else
+				{
+					j_x = 0;
+					j_width = tmpWidth - x_shift;
+				}
+
+				dimBlock = dim3(THREAD_COUNT, THREAD_COUNT);
+				dimGrid = dim3(((j_width) + dimBlock.x - 1) / dimBlock.x,
+							   ((i_height) + dimBlock.y - 1) / dimBlock.y);
+
+				cudaMemset(shifted_mtb[second_index * PYRAMID_LEVEL + k], 0,
+						   tmpNImageSize * sizeof(uint8_t));
+				cudaMemset(shifted_ebm[second_index * PYRAMID_LEVEL + k], 0,
+						   tmpNImageSize * sizeof(uint8_t));
+
+				uint8_t *xor_result;
+				cudaMalloc((void **)&xor_result,
+						   tmpNImageSize * sizeof(uint8_t));
+
+				uint8_t *after_first_and;
+				cudaMalloc((void **)&after_first_and,
+						   tmpNImageSize * sizeof(uint8_t));
+
+				uint8_t *after_second_and;
+				cudaMalloc((void **)&after_second_and,
+						   tmpNImageSize * sizeof(uint8_t));
+
+				int *err;
+				int error;
+				cudaMalloc((void **)&err, sizeof(int));
+
+				shift_Image<<<dimGrid, dimBlock, 0, stream>>>(
+					shifted_mtb[second_index * PYRAMID_LEVEL + k],
+					mtb[second_index * PYRAMID_LEVEL + k], tmpWidth,
+					tmpHeight, xs, ys, j_x, i_y, j_width, i_height);
+				shift_Image<<<dimGrid, dimBlock, 0, stream>>>(
+					shifted_ebm[second_index * PYRAMID_LEVEL + k],
+					ebm[second_index * PYRAMID_LEVEL + k], tmpWidth,
+					tmpHeight, xs, ys, j_x, i_y, j_width, i_height);
+
+				dimBlock = dim3(THREAD_COUNT, THREAD_COUNT);
+				dimGrid = dim3(((tmpWidth) + dimBlock.x - 1) / dimBlock.x,
+							   ((tmpHeight) + dimBlock.y - 1) / dimBlock.y);
+
+				XOR<<<dimGrid, dimBlock, 0, stream>>>(xor_result,
+													  mtb[first_index * PYRAMID_LEVEL + k],
+													  shifted_mtb[second_index * PYRAMID_LEVEL + k], tmpWidth,
+													  tmpNImageSize);
+
+				AND<<<dimGrid, dimBlock, 0, stream>>>(after_first_and,
+													  ebm[first_index * PYRAMID_LEVEL + k], xor_result,
+													  tmpWidth, tmpNImageSize);
+
+				AND<<<dimGrid, dimBlock, 0, stream>>>(after_second_and,
+													  shifted_ebm[second_index * PYRAMID_LEVEL + k],
+													  after_first_and, tmpWidth, tmpNImageSize);
+
+				count_Errors<<<32, 256, 0, stream>>>(after_second_and, err,
+													 tmpNImageSize);
+
+				cudaMemcpyAsync(&error, err, sizeof(int), cudaMemcpyDeviceToHost, stream); //CANNOT BE ASYNC, SYNC PLEASE
+				cudaStreamSynchronize(stream);
+
+				if (error < min_error)
+				{
+					offset_return_x = xs;
+					offset_return_y = ys;
+					min_error = error;
+				}
+				//cudaFree(err);
+			}
+		}
+	}
+
+	_arg->shiftp->x = curr_offset_x;
+	_arg->shiftp->y = curr_offset_y;
+
+	pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("Please supply the input images.");
+		return 0;
+	}
+
+	int width, height;
+	int img_count = argc - 1;
+	int num_threads = img_count;
+	pthread_t threads[num_threads];
+	cudaStream_t streams[img_count];
+
+	uint8_t *rgb_images[img_count];
+	uint8_t *d_rgb_images[img_count];
+	uint8_t *shifted_rgb_images[img_count];
+	uint8_t *mtb[PYRAMID_LEVEL * img_count];
+	uint8_t *ebm[PYRAMID_LEVEL * img_count];
+	uint8_t *shifted_mtb[PYRAMID_LEVEL * img_count];
+	uint8_t *shifted_ebm[PYRAMID_LEVEL * img_count];
+	float *gray_image[PYRAMID_LEVEL * img_count];
+
+	for (int i = 0; i < img_count; i++)
+	{
+		if (!(rgb_images[i] = stbi_load(argv[i + 1], &width, &height, NULL, 3)))
+		{
+			printf("Could not read image.");
+			return 0;
+		}
+	}
+
+	int nImageSize = width * height;				   //total pixel count of 1 channel of the RGB input image.
+	size_t sizeOfImage = nImageSize * sizeof(uint8_t); //size of 1 channel of the RGB source image, each pixel is converted to uint8_t.
+
+	//bu blok ustteki stbi_load ile 2 threadde ayni anda yapilir, start eventini bu yuzden altta baslatacagiz.
+	for (int i = 0; i < img_count; i++)
+	{
+		int tmpSizeOfImage = sizeOfImage;
+		int tmpWidth = width;
+		int tmpHeight = height;
+		int tmpNImageSize = nImageSize;
+
+		for (int j = 0; j < PYRAMID_LEVEL; j++, tmpSizeOfImage /= 4, tmpWidth /= 2, tmpHeight /= 2, tmpNImageSize /= 4)
+		{
+			cudaMalloc((void **)&(gray_image[i * PYRAMID_LEVEL + j]),
+					   tmpNImageSize * sizeof(float));
+			cudaMalloc((void **)&(mtb[i * PYRAMID_LEVEL + j]),
+					   tmpSizeOfImage);
+			cudaMalloc((void **)&(ebm[i * PYRAMID_LEVEL + j]),
+					   tmpSizeOfImage);
+			cudaMalloc((void **)&(shifted_mtb[i * PYRAMID_LEVEL + j]),
+					   tmpSizeOfImage);
+			cudaMalloc((void **)&(shifted_ebm[i * PYRAMID_LEVEL + j]),
+					   tmpSizeOfImage);
+		}
+	}
+
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start);
+
+	for (int i = 0; i < img_count; i++)
+	{
+		cudaStreamCreate(&(streams[i]));
+		cudaHostRegister(rgb_images[i], sizeOfImage * 3, 0);
+		cudaMalloc((void **)&d_rgb_images[i], sizeOfImage * 3);
+		cudaMemcpyAsync(d_rgb_images[i], rgb_images[i], sizeOfImage * 3,
+						cudaMemcpyHostToDevice, streams[i]);
+	}
+
+	for (int i = 0; i < img_count; i++)
+	{
+		dim3 dimBlock = dim3(THREAD_COUNT, THREAD_COUNT);
+		dim3 dimGrid = dim3((width + dimBlock.x - 1) / dimBlock.x,
+							(height + dimBlock.y - 1) / dimBlock.y);
+
+		convert_to_grayscale<<<dimGrid, dimBlock, 0, streams[i]>>>(
+			gray_image[i * PYRAMID_LEVEL],
+			d_rgb_images[i], nImageSize, width);
+	}
+
+	for (int i = 0; i < img_count; i++)
+	{
+		int tmpSizeOfImage = sizeOfImage;
+		int tmpWidth = width;
+		int tmpHeight = height;
+		int tmpNImageSize = nImageSize;
+
+		for (int j = 0; j < PYRAMID_LEVEL - 1; j++, tmpSizeOfImage /= 4, tmpWidth /= 2, tmpHeight /= 2, tmpNImageSize /= 4)
+		{
+			cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
+			cudaArray *cuArray;
+			cudaMallocArray(&cuArray, &channelDesc, tmpWidth,
+							tmpHeight);
+
+			cudaMemcpyToArray(cuArray, 0, 0,
+							  gray_image[i * PYRAMID_LEVEL + j],
+							  tmpNImageSize * sizeof(float),
+							  cudaMemcpyDeviceToDevice);
+
+			// Specify texture
+			struct cudaResourceDesc resDesc;
+			memset(&resDesc, 0, sizeof(resDesc));
+			resDesc.resType = cudaResourceTypeArray;
+			resDesc.res.array.array = cuArray;
+
+			// Specify texture object parameters
+			struct cudaTextureDesc texDesc;
+			memset(&texDesc, 0, sizeof(texDesc));
+			texDesc.addressMode[0] = cudaAddressModeClamp;
+			texDesc.addressMode[1] = cudaAddressModeClamp;
+			texDesc.filterMode = cudaFilterModeLinear;
+			texDesc.readMode = cudaReadModeElementType;
+			texDesc.normalizedCoords = 1;
+
+			// Create texture object
+			cudaTextureObject_t texObj = 0;
+			cudaCreateTextureObject(&texObj, &resDesc, &texDesc, NULL);
+
+			// Invoke kernel
+			dim3 dimBlock(THREAD_COUNT, THREAD_COUNT);
+			dim3 dimGrid((tmpWidth / 2 + dimBlock.x - 1) / dimBlock.x,
+						 (tmpHeight / 2 + dimBlock.y - 1) / dimBlock.y);
+
+			transform_kernel<<<dimGrid, dimBlock, 0, streams[i]>>>(
+				gray_image[i * PYRAMID_LEVEL + j + 1], texObj,
+				tmpWidth / 2, tmpHeight / 2);
+		}
+	}
+
+	for (int i = 0; i < img_count; i++)
+	{
+		int tmpSizeOfImage = sizeOfImage;
+		int tmpWidth = width;
+		int tmpHeight = height;
+		int tmpNImageSize = nImageSize;
+
+		for (int j = 0; j < PYRAMID_LEVEL; j++, tmpSizeOfImage /= 4, tmpWidth /= 2, tmpHeight /= 2, tmpNImageSize /= 4)
+		{
+			dim3 dimBlock(BLOCK_SIZE);
+			dim3 dimGrid(32);
+
+			int *hist;
+			cudaMalloc((void **)&hist, BLOCK_SIZE * sizeof(int) * 32);
+
+			histogram_smem_atomics<<<dimGrid, dimBlock, 0, streams[i]>>>(
+				gray_image[i * PYRAMID_LEVEL + j], hist,
+				tmpNImageSize);
+
+			histogram_final_accum<<<1, 256, 0, streams[i]>>>(BLOCK_SIZE * dimGrid.x, hist);
+
+			//TODO SUREKLI MEDIAN MALLOCLAMASIN, ARRAY ACALIM EN BASTAN
+			//TODO BURALARI OPTIMIZE ET
+			int *median;
+			cudaMalloc((void **)&median, sizeof(int));
+			find_Median<<<1, 1, 0, streams[i]>>>(tmpNImageSize, hist, median);
+
+			dimBlock = dim3(THREAD_COUNT, THREAD_COUNT);
+			dimGrid = dim3((tmpWidth + dimBlock.x - 1) / dimBlock.x,
+						   (tmpHeight + dimBlock.y - 1) / dimBlock.y);
+
+			find_Mtb_Ebm<<<dimGrid, dimBlock, 0, streams[i]>>>(
+				gray_image[i * PYRAMID_LEVEL + j], median,
+				mtb[i * PYRAMID_LEVEL + j],
+				ebm[i * PYRAMID_LEVEL + j], tmpHeight, tmpWidth);
+		}
+	}
+
+	//cudaDeviceSynchronize(); //wait for all streams to finish up.
+
+	// for (int var = 0; var < img_count; ++var)
+	// {
+	// 	cudaHostUnregister(rgb_images[var]);
+	// }
+
+	//**********************************************************************************************************************************
+	shift_pair all_shifts[img_count];
+
+	int mid_img_index = img_count / 2; //TODO belki +1.
+
+	for (int m = mid_img_index - 1; m >= 0; --m)
+	{
+		arguments *args = (arguments *)malloc(sizeof(arguments));
+		args->shiftp = &(all_shifts[m]);
+		args->stream = streams[m];
+		args->first_ind = m + 1;
+		args->second_ind = m;
+		args->width = width;
+		args->height = height;
+		args->mtb = mtb;
+		args->ebm = ebm;
+		args->shifted_mtb = shifted_mtb;
+		args->shifted_ebm = shifted_ebm;
+
+		//cout << "part1 thread create ediyoz..." << endl;
+		int rc = pthread_create(&threads[m], NULL, calculateOffset,
+								(void *)args);
+
+		if (rc)
+		{
+			cout << "Error:unable to create thread," << rc << endl;
+			exit(-1);
+		}
+	}
+	for (int m = mid_img_index + 1; m < img_count; ++m)
+	{
+
+		arguments *args = (arguments *)malloc(sizeof(arguments));
+		args->shiftp = &(all_shifts[m]);
+		args->stream = streams[m];
+		args->first_ind = m - 1;
+		args->second_ind = m;
+		args->width = width;
+		args->height = height;
+		args->mtb = mtb;
+		args->ebm = ebm;
+		args->shifted_mtb = shifted_mtb;
+		args->shifted_ebm = shifted_ebm;
+
+		//cout << "part2 thread create ediyoz..." << endl;
+		int rc = pthread_create(&threads[m], NULL, calculateOffset,
+								((void *)args));
+
+		if (rc)
+		{
+			cout << "Error:unable to create thread," << rc << endl;
+			exit(-1);
+		}
+	}
+
+	//cudaDeviceSynchronize();
+
+	void *status;
+	for (int var = 0; var < num_threads; ++var)
+	{
+		if (var != mid_img_index)
+			pthread_join(threads[var], &status);
+	}
+	//cout << "thread joinler bitti..." << endl;
+
+	//cudaDeviceSynchronize();
+
+	//cout << " ilk parttaki imajlari shiftliyoruz tek tek ..." << endl;
+
+	int eskiTotalX = 0, eskiTotalY = 0;
+	for (int m = mid_img_index - 1; m >= 0; --m)
+	{
+
+		int x_shift = all_shifts[m].x + eskiTotalX;
+		int y_shift = all_shifts[m].y + eskiTotalY; //TODO check those
+
+		int tmpWidth = width;
+		int tmpHeight = height;
+		int tmpNImageSize = tmpWidth * tmpHeight;
+
+
+		//TODO bunu da bastn hallet
+		cudaMalloc((void **)&shifted_rgb_images[m], 3 * tmpNImageSize);
+		cudaMemset(shifted_rgb_images[m], 0,
+				   3 * tmpNImageSize * sizeof(uint8_t));
+
+		int j_x, i_y, j_width, i_height;
+
+		if (y_shift < 0)
+		{ //height i
+			i_y = -y_shift;
+			i_height = tmpHeight;
+		}
+		else
+		{
+			i_y = 0;
+			i_height = tmpHeight - y_shift;
+		}
+
+		if (x_shift < 0)
+		{ //width j
+			j_x = -x_shift;
+			j_width = tmpWidth;
+		}
+		else
+		{
+			j_x = 0;
+			j_width = tmpWidth - x_shift;
+		}
+
+		dim3 dimBlock = dim3(THREAD_COUNT, THREAD_COUNT);
+		dim3 dimGrid = dim3(((j_width) + dimBlock.x - 1) / dimBlock.x,
+							((i_height) + dimBlock.y - 1) / dimBlock.y);
+
+		RGB_shift_Image<<<dimGrid, dimBlock, 0, streams[m]>>>(
+			shifted_rgb_images[m], d_rgb_images[m], tmpWidth, tmpHeight,
+			x_shift, y_shift, j_x, i_y, j_width, i_height);
+
+		eskiTotalX += all_shifts[m].x;
+		eskiTotalY += all_shifts[m].y;
+		//cout << "   shiftledik: x,y " << eskiTotalX << " " << eskiTotalY << endl;
+	}
+
+	eskiTotalX = 0;
+	eskiTotalY = 0;
+	for (int m = mid_img_index + 1; m < img_count; ++m)
+	{
+
+		int x_shift = all_shifts[m].x + eskiTotalX;
+		int y_shift = all_shifts[m].y + eskiTotalY; //TODO check those
+
+		int tmpWidth = width;
+		int tmpHeight = height;
+		int tmpNImageSize = tmpWidth * tmpHeight;
+
+
+		//TODO bunu da bastn hallet
+		cudaMalloc((void **)&shifted_rgb_images[m], 3 * tmpNImageSize);
+		cudaMemset(shifted_rgb_images[m], 0,
+				   3 * tmpNImageSize * sizeof(uint8_t));
+
+		int j_x, i_y, j_width, i_height;
+
+		if (y_shift < 0)
+		{ //height i
+			i_y = -y_shift;
+			i_height = tmpHeight;
+		}
+		else
+		{
+			i_y = 0;
+			i_height = tmpHeight - y_shift;
+		}
+
+		if (x_shift < 0)
+		{ //width j
+			j_x = -x_shift;
+			j_width = tmpWidth;
+		}
+		else
+		{
+			j_x = 0;
+			j_width = tmpWidth - x_shift;
+		}
+
+		dim3 dimBlock = dim3(THREAD_COUNT, THREAD_COUNT);
+		dim3 dimGrid = dim3(((j_width) + dimBlock.x - 1) / dimBlock.x,
+							((i_height) + dimBlock.y - 1) / dimBlock.y);
+
+		RGB_shift_Image<<<dimGrid, dimBlock, 0, streams[m]>>>(
+			shifted_rgb_images[m], d_rgb_images[m], tmpWidth, tmpHeight,
+			x_shift, y_shift, j_x, i_y, j_width, i_height);
+
+		eskiTotalX += all_shifts[m].x;
+		eskiTotalY += all_shifts[m].y;
+		//cout << "   shiftledik: x,y " << eskiTotalX << " " << eskiTotalY << endl;
+	}
+
+	cudaDeviceSynchronize();
+
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	cout << "Time took: "<<milliseconds << " ms" << endl;
+
+	//print original grayscale img
+	//	stbi_write_png(path, width, height, 3, rgb_images[mid_img_index], width*3);
+
+	int tmpNImageSize = height * width;
+	int tmpHeight = height;
+	int tmpWidth = width;
+
+	for (int var = 0; var < img_count; ++var)
+	{
+
+		if (var == mid_img_index)
+		{
+			continue;
+		}
+
+		char str[12];
+		sprintf(str, "%d%d%d.jpg", var+1,var+1,var+1);
+		char path[80] = "./output/";
+		strcat(path, str);
+
+		uint8_t *tmpmtb = (uint8_t *)malloc(
+			sizeof(uint8_t) * tmpNImageSize * 3);
+		cudaMemcpy(tmpmtb, shifted_rgb_images[var],
+				   sizeof(uint8_t) * tmpNImageSize * 3, cudaMemcpyDeviceToHost);
+
+		stbi_write_jpg(path, tmpWidth, tmpHeight, 3, tmpmtb, tmpWidth * 3);
+	}
+	char str[12];
+	sprintf(str, "%d%d%d.jpg", mid_img_index+1,mid_img_index+1,mid_img_index+1);
+	char path[80] = "./output/";
+	strcat(path, str);
+
+	uint8_t *tmpmtb = (uint8_t *)malloc(sizeof(uint8_t) * tmpNImageSize * 3);
+	cudaMemcpy(tmpmtb, d_rgb_images[mid_img_index],
+			   sizeof(uint8_t) * tmpNImageSize * 3, cudaMemcpyDeviceToHost);
+
+	stbi_write_jpg(path, tmpWidth, tmpHeight, 3, tmpmtb, tmpWidth * 3);
+
+	printf("Done. Wrote output images to output folder.\n");
+
+	return 0;
+}
---
+++ kernels.h
@@ -0,0 +1,279 @@
+#ifndef __KERNELS_H__
+#define __KERNELS_H__
+
+#include "device_launch_parameters.h"
+#include "cuda_runtime.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "./stb-master/stb_image.h"
+
+#define BLOCK_SIZE 256
+#define COLOR 256
+#define PYRAMID_LEVEL 6
+
+typedef struct shift_pair
+{
+	shift_pair()
+	{
+		x = 0;
+		y = 0;
+	}
+	shift_pair(int _x, int _y)
+	{
+		x = _x;
+		y = _y;
+	}
+	shift_pair(const shift_pair &rhs)
+	{
+		x = rhs.x;
+		y = rhs.y;
+	}
+
+	int x;
+	int y;
+} shift_pair;
+
+typedef struct arg_struct
+{
+	shift_pair *shiftp;
+	cudaStream_t stream;
+	int first_ind;
+	int second_ind;
+	int width;
+	int height;
+	uint8_t **mtb;
+	uint8_t **ebm;
+	uint8_t **shifted_mtb;
+	uint8_t **shifted_ebm;
+} arguments;
+
+// Simple transformation kernel
+__global__ void transform_kernel(float *output, cudaTextureObject_t texObj,
+								int width, int height)
+{
+
+	// Calculate normalized texture coordinates
+	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	float u = x / (float)width;
+	float v = y / (float)height;
+
+	// Read from texture and write to global memory
+
+	output[y * width + x] = tex2D<float>(texObj, u, v);
+}
+
+__global__ void convert_to_grayscale(float *gray, uint8_t *img, int size, int width)
+{
+
+	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+	int idx = y * width + x;
+
+	if (idx < size)
+	{
+		int index = idx * 3;
+		gray[idx] = (54 * img[index] + 183 * img[index + 1] + 19 * img[index + 2]) / 256.0f;
+	}
+}
+
+__global__ void histogram_smem_atomics(const float *input, int *out, int size)
+{
+	__shared__ int smem[BLOCK_SIZE];
+
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x * BLOCK_SIZE + tid;
+	unsigned int gridSize = BLOCK_SIZE * gridDim.x;
+
+	smem[tid] = 0;
+	__syncthreads();
+
+	//	uint8_t pixel;
+	while (i < size)
+	{
+
+		//		pixel = input[i];
+		atomicAdd(&smem[(int)input[i]], 1);
+
+		i += gridSize;
+	}
+	__syncthreads();
+
+	out[blockIdx.x * BLOCK_SIZE + tid] = smem[tid];
+}
+
+__global__ void histogram_final_accum(int n, int *out)
+{
+	int tid = threadIdx.x;
+	int i = tid;
+	int total = 0;
+
+	while (i < n)
+	{
+		total += out[i];
+		i += BLOCK_SIZE;
+	}
+	__syncthreads();
+
+	out[tid] = total;
+}
+
+__global__ void find_Median(int n, int *hist, int *median)
+{
+	int half_way = n / 2;
+	int sum = 0;
+
+	for (int k = 0; k < COLOR; k++)
+	{
+		sum += hist[k];
+		if (sum > half_way)
+		{
+			*median = k;
+			return;
+		}
+	}
+}
+
+__global__ void find_Mtb_Ebm(const float *input, int *median, uint8_t *_mtb,
+							 uint8_t *_ebm, int _height, int _width)
+{
+
+	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned int idx = y * _width + x;
+
+	if (input[idx] < (*median - 4) || input[idx] > (*median + 4))
+	{
+
+		_ebm[idx] = 255;
+	}
+	else
+	{
+		_ebm[idx] = 0;
+	}
+
+	if (input[idx] < *median)
+	{
+
+		_mtb[idx] = 0;
+	}
+	else
+	{
+		_mtb[idx] = 255;
+	}
+}
+
+__global__ void AND(uint8_t *output, uint8_t *left, uint8_t *right, int width,
+					int size)
+{
+
+	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	int index = y * width + x;
+	if (index < size)
+	{
+		output[index] = left[index] & right[index];
+	}
+}
+
+__global__ void XOR(uint8_t *output, uint8_t *left, uint8_t *right, int width,
+					int size)
+{
+
+	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	int index = y * width + x;
+	if (index < size)
+	{
+		output[index] = left[index] ^ right[index];
+	}
+}
+
+__global__ void count_Errors(const uint8_t *input, int *out, int size)
+{
+	__shared__ int count;
+
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x * BLOCK_SIZE + tid;
+	unsigned int gridSize = BLOCK_SIZE * gridDim.x;
+
+	count = 0;
+
+	while (i < size)
+	{
+
+		if (input[i] == 255)
+		{
+			atomicAdd(&count, 1);
+		}
+
+		i += gridSize;
+	}
+
+	if (tid == 0)
+	{
+		atomicAdd(out, count);
+	}
+}
+
+__global__ void shift_Image(uint8_t *output, uint8_t *input, int width,
+							int height, int x_shift, int y_shift, int j_x, int i_y, int j_width,
+							int i_height)
+{
+
+	int j = blockIdx.x * blockDim.x + threadIdx.x + j_x;
+	int i = blockIdx.y * blockDim.y + threadIdx.y + i_y;
+
+	unsigned int input_index = i * width + j;
+
+	unsigned int output_index = y_shift * width + x_shift + i * width + j;
+
+	if (i < i_height && j < j_width)
+	{
+		output[output_index] = input[input_index];
+	}
+}
+
+__global__ void RGB_shift_Image(uint8_t *output, uint8_t *input, int width,
+								int height, int x_shift, int y_shift, int j_x, int i_y, int j_width,
+								int i_height)
+{
+
+	int j = blockIdx.x * blockDim.x + threadIdx.x + j_x;
+	int i = blockIdx.y * blockDim.y + threadIdx.y + i_y;
+
+	unsigned int input_index = i * width + j;
+	input_index *= 3;
+
+	unsigned int output_index = y_shift * width + x_shift + i * width + j;
+	output_index *= 3;
+
+	if (i < i_height && j < j_width)
+	{
+		output[output_index++] = input[input_index++];
+		output[output_index++] = input[input_index++];
+		output[output_index] = input[input_index];
+	}
+}
+
+__global__ void finalShift(uint8_t *output, float *input, int width, int height,
+						   int x_shift, int y_shift, int j_x, int i_y, int j_width, int i_height)
+{
+
+	int j = blockIdx.x * blockDim.x + threadIdx.x + j_x;
+	int i = blockIdx.y * blockDim.y + threadIdx.y + i_y;
+
+	unsigned int input_index = i * width + j;
+
+	unsigned int output_index = y_shift * width + x_shift + i * width + j;
+
+	if (i < i_height && j < j_width)
+	{
+		output[output_index] = input[input_index];
+	}
+}
+
+#endif
